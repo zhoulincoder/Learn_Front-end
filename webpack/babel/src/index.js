b83568d59a3423c5d22a9db89561dc51f195f767
HenleyZ <ZhouLin1620@163.com>
2019-05-20 16:26:05 +0800

    babel工作流

---
+++ webpack/babel/src/index.js
@@ -0,0 +1,11 @@
+const a = 1, 
+  b = 2,
+  c = a + b;
+console.log(c);
+
+// [1,2,3].map(item => `${item}item`)
+
+// //es6的大部分功能，es5能实现，但是不够精炼
+// let xlz = {name: '范玲子', hometown: '吉林'}
+// xlz = {...xlz, 'company': '新浪'}
+// console.log(xlz)
