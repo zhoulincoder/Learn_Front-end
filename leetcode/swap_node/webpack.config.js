32a0e20b3cf93e72fcc61b3fb20ad2896da8b825
HenleyZ <ZhouLin1620@163.com>
2019-05-28 22:59:01 +0800

    update

--- leetcode/swap_node/webpack.config.js
+++ leetcode/swap_node/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
 
   entry: './src/index',
   output: {
-    path : path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: '[name].js'
   },
 
