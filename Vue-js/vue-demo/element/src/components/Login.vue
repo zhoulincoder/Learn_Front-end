0e56791d80ce9a284bfe9dd86a5823ff7a8e1c71
HenleyZ <ZhouLin1620@163.com>
2019-05-29 22:57:32 +0800

    深入学习vue

---
+++ Vue-js/vue-demo/element/src/components/Login.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="login">
+    <div class="container">
+      <p class="title">后台管理系统</p>
+      <div class="msg mt20">
+        <el-input v-model="user" placeholder="请输入用户名"></el-input>
+      </div>
+      <div class="msg mt20">
+        <el-input placeholder="请输入密码" v-model="password" show-password></el-input>
+      </div>
+      <div class="btn mt20">
+        <el-button type="primary" @click="login">登陆</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      user: '',
+      password: ''
+    }
+  },
+  methods: {
+    login () {
+      if( this.user && this.password) {
+        //数据存浏览器本地
+        localStorage.setItem('userName', this.user)
+        localStorage.setItem('userPassord', this.password)
+      } 
+    }
+  }
+}
+</script>
+<style scoped>
+  .login{
+    margin-top: 150px;
+    width: 100%;
+  }
+  .container{
+    width: 300px;
+    border-radius: 4px;  
+    margin: 0 auto;
+    box-shadow: 0 0 10px #cac6c6;
+    padding: 50px;
+  }
+  .title{
+    font-size: 26px;
+    font-weight: bold;
+    color: #505458;
+  }
+  .btn button{
+    width: 100%;
+  }
+  .mt20{
+    margin-top: 20px;
+  }
+</style>
