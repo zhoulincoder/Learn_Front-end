2cd1ea44ee8727744a50ba58770e942bbc09f597
HenleyZ <ZhouLin1620@163.com>
2019-06-05 14:18:59 +0800

    6.5 vue

--- Vue-js/vue-demo/element/src/components/Login.vue
+++ Vue-js/vue-demo/element/src/components/Login.vue
@@ -29,6 +29,13 @@ export default {
         //数据存浏览器本地
         localStorage.setItem('userName', this.user)
         localStorage.setItem('userPassord', this.password)
+        //字符串  路由跳转
+        //this.$router.push('/main')
+        //对象的形式 
+        // this.$router.push({path:'/main'})
+        //编程式跳转
+        this.$router.push({name: 'Main', params:{userId: 1}})
+
       } 
     }
   }
