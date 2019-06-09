af5fd3cbabce1a76ae7cc24e99868f91fe0fb901
HenleyZ <ZhouLin1620@163.com>
2019-06-09 14:44:29 +0800

    new

--- leetcode/30seconds/demo3.js
+++
@@ -1,5 +0,0 @@
-
-const words = (str,pattern=/[^a-zA-Z-]+/) => {
-  str.split(pattern)
-  console.log(words('I love javascript!!'));
-}
--- leetcode/30seconds/index.js
+++
@@ -1,4 +0,0 @@
-const htmlStr = '<p><em>lorem</em><strong>ipsum</strong></p>';
-// lorem ipsum strip tags
-// 正则 replace splice 规则
-const stripHTMLTagps = str => str.replace(/<[^>]*>/g, '');
