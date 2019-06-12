4be4d58b940c60aacf5f10a90a22b469ff3b89d3
HenleyZ <ZhouLin1620@163.com>
2019-06-12 22:53:52 +0800

    全栈开发

--- node/koa-blog/index.js
+++ node/koa-blog/index.js
@@ -1,8 +1,13 @@
 const Koa = require('koa')
-// 配置模板引擎ejs
+
 const views = require('koa-views')
+// 配置模板引擎ejs
 const koaStatic = require('koa-static')
+// 配置静态资源
+const bodyParser = require('koa-bodyparser')
 const path = require('path')
+
+//路由部分
 const app = new Koa()
 const signinRouter = require('./routers/signin.js')
 const {PORT} = require('./config/default.js')
@@ -12,6 +17,9 @@ app.use(views(path.join(__dirname, './views'), {
   extension: 'ejs'
 }))  //     指定路径   和 扩展名
 
+app.use(bodyParser({
+  formLimit: '1mb'
+}))
 app.use(signinRouter.routes())
 app.listen(PORT, () => {
   console.log( `server is running ${PORT}`)
