18f37986583431b07ddec894dc1836b416d56013
HenleyZ <zhoulin1620@163.com>
2019-06-25 23:02:10 +0800

    自定义弹窗组件

---
+++ react/ant-toast-component/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import toast from './toast/index'
+
+const openNotificationWithIcon = type => {
+  toast[type]('这是提示的内容');
+};
+
+function App() {
+  return (
+    <>
+    <button onClick={() => openNotificationWithIcon('success')}>Success</button>
+    <button onClick={() => openNotificationWithIcon('info')}>Info</button>
+    <button onClick={() => openNotificationWithIcon('warning')}>Warning</button>
+    <button onClick={() => openNotificationWithIcon('error')}>Error</button>
+    </>
+  );
+}
+
+export default App;
+
