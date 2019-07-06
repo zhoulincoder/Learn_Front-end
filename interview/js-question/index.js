07b43792b784ccfe396f9705e7d724cd79608673
HenleyZ <zhoulin1620@163.com>
2019-07-06 22:58:29 +0800

    总结

--- interview/js-question/index.js
+++ interview/js-question/index.js
@@ -146,3 +146,24 @@
 
 // console.log(arr);
 
+function Fn() {   // 内部语句: this.prototype = {}  
+  
+  }  
+  // 1. 每个函数function都有一个prototype，即显式原型属性, 默认指向一个空的Object对象  
+  console.log(Fn.prototype)  
+  // 2. 每个实例对象都有一个__proto__，可称为隐式原型  
+  //创建实例对象  
+  var fn = new Fn()  // 内部语句: this.__proto__ = Fn.prototype  
+  console.log(fn.__proto__)  
+  // 3. 对象的隐式原型的值为其对应构造函数的显式原型的值  
+  console.log(Fn.prototype===fn.__proto__) // true  
+  //给原型添加方法  
+  Fn.prototype.test = function () {  
+    console.log('test()')  
+  }  
+  //通过实例调用原型的方法  
+  fn.test()
+  console.log(Object.__proto__ === Function.prototype);
+  console.log(Fn.prototype == Function.constructor);
+  
+  
--- interview/js-question/q1.js
+++ interview/js-question/q1.js
@@ -2,7 +2,7 @@
 function _new() {
   //new 方法的实现
   let target = {}
-  let [constructor, ...args] = [...arguments]
+  let [constructor, ...args] = [...arguments]  // 这句
   target.__proto__ = constructor.prototype  // 新对象的隐式链   执行原型连接
   let result = constructor.apply(target, args)  //执行构造函数，将函数的属性或者方法添加到创建的空对象上
   if (result && (typeof (result) === 'object' || typeof (result) === 'function')) {
