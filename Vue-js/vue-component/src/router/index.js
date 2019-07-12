0f4244471cda8768c6d1a7f78d815af40b192c36
HenleyZ <zhoulin1620@163.com>
2019-07-12 09:07:14 +0800

    vue动态加载组件

--- Vue-js/vue-component/src/router/index.js
+++ Vue-js/vue-component/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Display from '@/view/Display'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Display',
+      component: Display
     }
   ]
 })
